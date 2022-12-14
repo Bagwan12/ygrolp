497182887e70d29fc7182de7406e69c1971ecf67
Bagwan12 <119614584+Bagwan12@users.noreply.github.com>
2022-12-14 04:44:56 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import {NavigationContainer } from '@react-navigation/native'
+import { StackNavigation } from "./screens/stack"
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StackNavigation/>
+    </NavigationContainer>
+   
+  );
+}
